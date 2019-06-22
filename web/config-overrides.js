f79e9cf2bd5deda806a56da1b082826380a888b3
Brady <acheng_69@163.com>
2019-06-22 15:59:43 +0800

    配置移动端、按需引入 antd-mobile、自定义主题

---
+++ web/config-overrides.js
@@ -0,0 +1,10 @@
+/**
+ * Created by brady on 2019-06-22.
+ */
+const { override, fixBabelImports, addLessLoader } = require('customize-cra');
+
+module.exports = override(
+  fixBabelImports('import', { libraryName: 'antd-mobile', libraryDirectory: 'es', style: true, }),
+  addLessLoader({ javascriptEnabled: true, modifyVars: { "@brand-primary": "#3BBE64", "@brand-primary-tap": "#3BBE64", }
+  })
+)
--- web/src/App.js
+++ web/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'antd-mobile'
 
 function App() {
   return (
     <div className="App">
+      <Button type="primary">按钮</Button>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
