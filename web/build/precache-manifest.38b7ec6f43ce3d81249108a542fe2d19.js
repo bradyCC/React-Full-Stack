f84559ccad7691567cd9a11b0488ff327a200935
Brady <acheng_69@163.com>
2019-06-22 21:54:07 +0800

    build打包

---
+++ web/build/precache-manifest.38b7ec6f43ce3d81249108a542fe2d19.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "560e6dc4c7df92d5b64c9d25e863307e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8bb30facf0aa81d0a62c",
+    "url": "/static/css/2.5498182b.chunk.css"
+  },
+  {
+    "revision": "7011c598aa8fa332b88a",
+    "url": "/static/css/main.51fc25df.chunk.css"
+  },
+  {
+    "revision": "8bb30facf0aa81d0a62c",
+    "url": "/static/js/2.9ecc766a.chunk.js"
+  },
+  {
+    "revision": "7011c598aa8fa332b88a",
+    "url": "/static/js/main.8285606f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ba1f87ecbc18b2f0a8b6e29e8676ac82",
+    "url": "/static/media/logo.ba1f87ec.png"
+  }
+]);
