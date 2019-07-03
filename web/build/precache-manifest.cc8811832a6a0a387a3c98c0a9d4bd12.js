3633dd8be4dc7ed19b12ab6465bbd9a0aa492804
brady <acheng_69@163.com>
2019-07-03 13:53:14 +0800

    编译打包

---
+++ web/build/precache-manifest.cc8811832a6a0a387a3c98c0a9d4bd12.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "606953a90b010f7eaa672612674c19e4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bb81294b7d54949c972b",
+    "url": "./static/css/2.1496323d.chunk.css"
+  },
+  {
+    "revision": "74ceccb0916c70fc01b5",
+    "url": "./static/css/main.535486fb.chunk.css"
+  },
+  {
+    "revision": "bb81294b7d54949c972b",
+    "url": "./static/js/2.a6306853.chunk.js"
+  },
+  {
+    "revision": "74ceccb0916c70fc01b5",
+    "url": "./static/js/main.fbeff34c.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "034f3d039e64d4bd193e12692b50539e",
+    "url": "./static/media/avatar2.034f3d03.jpg"
+  },
+  {
+    "revision": "a9602e7ac6a642039d8f1b2d86b83a9c",
+    "url": "./static/media/logo.a9602e7a.png"
+  }
+]);
