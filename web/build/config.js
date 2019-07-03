dc5c0f5f7b55994200ab6cde6004ade081e1a5e5
brady <acheng_69@163.com>
2019-07-03 13:40:31 +0800

    编译打包

---
+++ web/build/config.js
@@ -0,0 +1,8 @@
+/**
+ * Created by brady on 2019/7/3.
+ */
+window.Domain = {
+  IpAddress: 'http://192.168.1.119:3000', // 公司
+  // IpAddress: 'http://192.168.1.108:3000', // 虚拟机
+  // IpAddress: 'http://192.168.1.101:3000', // 家
+}
